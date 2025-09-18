dae7c06bd7d702cc50ffb15334c2266e14cba0e7
vikassfteng <vikas.sft.engg.1@gmail.com>
2025-09-18 21:48:17 +0530

    Kafka streamline

---
+++ kafka/worker/worker.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/IBM/sarama"
+)
+
+func main() {
+	topic := "comments"
+	worker, err := connectConsumer([]string{"localhost:29092"})
+
+	if err != nil {
+		panic(err)
+	}
+
+	consumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Consumer is ready")
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	msgcount := 0
+	doneCh := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case err := <-consumer.Errors():
+				fmt.Println(err)
+			case msg := <-consumer.Messages():
+				msgcount++
+				fmt.Printf("Received message count: %d: | Topic (%s) | Message(%s)\n", msgcount, msg.Topic, string(msg.Value))
+			case <-sigchan:
+				fmt.Println("Interrupt is detected")
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+	<-doneCh
+	fmt.Println("Processed", msgcount, "messages")
+	if err := worker.Close(); err != nil {
+		panic(err)
+	}
+}
+
+func connectConsumer(brokersUrl []string) (sarama.Consumer, error) {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	conn, err := sarama.NewConsumer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
